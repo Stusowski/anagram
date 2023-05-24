cf9f339d9ce72dfcd747e8286388e3f5a43b78dc
Stusowski <126696159+Stusowski@users.noreply.github.com>
2023-05-24 23:33:33 +0000

    anagram

--- Main.java
+++ Main.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
+    System.out.println("Sprawdźmy, czy podane przez Ciebie wyrazy będą anagramami!");
     System.out.println("Podaj pierwsze słowo:");
     String first = in.nextLine();
     char[] a = first.toCharArray();
     System.out.println("Podaj drugie słowo:");
     String second = in.nextLine();
     char[] b = second.toCharArray();
-    int spr = 0;
-    for(int i=0;i<a.length;i++){
-      for(int j=0;j<b.length;j++){
-        if(a[i]==b[j]){
-        spr=spr+1;
-        }
-      }
-    }
-    if(spr==a.length){
-      System.out.println("Wyrazy są anagramami.");
+    if(a.length!=b.length){
+      System.out.println("Wyrazy nie są anagramami.");
     }
     else{
-      System.out.println("Wyrazy nie są anagramami.");
+      Arrays.sort(a);
+      Arrays.sort(b);
+      int spr = 0;
+      for(int i=0;i<a.length;i++){
+        if(a[i]==b[i]){
+          spr=spr+1;
+        }
+      }
+      if(spr==a.length){
+        System.out.println("Wyrazy są anagramami.");
+      }
+      else{
+        System.out.println("Wyrazy nie są anagramami.");
+      }
     }
   }
 }
